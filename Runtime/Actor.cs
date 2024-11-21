abaf2b4b7435ea410f766ce44ee986b51a830f7e
Onur ÖZEL <sumotoma_onur@hotmail.com>
2024-11-21 17:34:44 +0300

    Add files via upload

--- Runtime/Actor.cs
+++ Runtime/Actor.cs
@@ -5,6 +5,16 @@ namespace RedShoreGames.EventSystem
     public abstract class Actor<T> : MonoBehaviour where T : MonoBehaviour 
     {
         public EventManager Manager => EventManager.Instance;
+        
+        protected virtual void OnEnable()
+        {
+            Listen(true);
+        }
+
+        protected virtual void OnDisable()
+        {
+            Listen(false);
+        }
 
         protected virtual void Push(string eventName, params object[] arguments)
         {
